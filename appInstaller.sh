1f9e9a6129102577458529a2e506dcd9d45dd583
Abhilash Joshi <45088116+alphaspear@users.noreply.github.com>
2023-09-05 22:02:09 +0530

    Update appInstaller.sh

--- appInstaller.sh
+++ appInstaller.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install brave-browser sublime-text docker-ce docker-ce-cli containerd.io docker-buildx-plugin gnome-tweaks insomnia docker-compose-plugin code -y
+sudo apt install brave-browser sublime-text docker-ce docker-ce-cli containerd.io docker-buildx-plugin spotify-client gnome-tweaks insomnia docker-compose-plugin code -y
