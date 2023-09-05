0412b225910e0442d8df8a66c3613e03575e5efd
Abhilash Joshi <45088116+alphaspear@users.noreply.github.com>
2023-09-05 21:59:31 +0530

    Create downloadAndInstall.sh

---
+++ downloadAndInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://download3.vmware.com/software/WKST-PLAYER-1702/VMware-Player-Full-17.0.2-21581411.x86_64.bundle
+sudo ./VMware-Play*
+wget https://repo.anaconda.com/archive/Anaconda3-2023.07-2-Linux-x86_64.sh
+chmod +x Anaconda3-2023.07-2-Linux-x86_64.sh
+./Anaconda3-2023.07-2-Linux-x86_64.sh
