a86982eecaefc629ac661bb468cc1ca2a6180822
Abhilash Joshi <45088116+alphaspear@users.noreply.github.com>
2023-09-05 23:01:41 +0530

    added maven

--- appInstaller.sh
+++ appInstaller.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install brave-browser sublime-text dbeaver-ce docker-ce docker-ce-cli containerd.io docker-buildx-plugin spotify-client gnome-tweaks insomnia docker-compose-plugin code htop telegram-desktop pgadmin4-desktop qbittorrent vlc python3-pip zsh -y
+sudo apt install brave-browser sublime-text dbeaver-ce docker-ce docker-ce-cli containerd.io docker-buildx-plugin spotify-client gnome-tweaks insomnia docker-compose-plugin code htop telegram-desktop pgadmin4-desktop qbittorrent vlc python3-pip zsh maven -y
